2abea830facda20ab33713107cd537fd3ac16d53
Ilya <ilyasergeev2626@gmail.com>
2025-03-13 22:59:32 +0300

    add function createAuction and struct

--- contracts/AuctionVickrey.sol
+++ contracts/AuctionVickrey.sol
@@ -1,34 +1,50 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.28;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+contract AuctionVickrey is Ownable {
 
-    event Withdrawal(uint amount, uint when);
+    constructor() Ownable(msg.sender) {}
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+    event auctionIsCreated(uint256 _auctionId, uint256 _startTime, uint256 _revealTime, uint256 _endTime);
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    error auctionAlreadyExist();
+    error incorrectTimeLine();
+
+    struct Auction {
+        //@notice user's address => hash of user's bid (keccak256(ammount, secretPhrase))
+        mapping(address => bytes32) hashedBids;
+        //@notice user's address => revealed bid
+        mapping(address => uint256) bids;
+
+        uint256 auctionId;
+        uint256 startTime;
+        uint256 revealTime;
+        uint256 endTime;
+        
+        bool isExist;
+        bool isAuctionEnded;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    mapping(uint256 => Auction) private auctions;
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+    function createAuction(uint256 _auctionId, uint256 _revealTime, uint256 _endTime) external onlyOwner {
+        require(_endTime > _revealTime, incorrectTimeLine());
+        require(auctions[_auctionId].isExist == false, auctionAlreadyExist());
+        Auction storage auctionInstance = auctions[_auctionId];
+        auctionInstance.startTime = block.timestamp;
+        auctionInstance.revealTime = block.timestamp + _revealTime;
+        auctionInstance.endTime = block.timestamp + _endTime;
+        auctionInstance.isExist = true;
+        emit auctionIsCreated(_auctionId, auctionInstance.startTime, _revealTime, _endTime);
     }
+
+    function setBid() external {}
+
+    function revealBid() external {}
+
+    function refundBid() external {}
+
 }
