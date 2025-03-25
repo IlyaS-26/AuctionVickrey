2dededc92d830605e48709d26c248c1c967b5d4c
Ilya <ilyasergeev2626@gmail.com>
2025-03-25 21:03:33 +0300

    Added some tests

--- contracts/AuctionVickrey.sol
+++ contracts/AuctionVickrey.sol
@@ -5,10 +5,14 @@ pragma solidity ^0.8.28;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract AuctionVickrey is Ownable {
-
     constructor() Ownable(msg.sender) {}
 
-    event auctionIsCreated(uint256 _auctionId, uint256 _startTime, uint256 _revealTime, uint256 _endTime);
+    event auctionIsCreated(
+        uint256 _auctionId,
+        uint256 _startTime,
+        uint256 _revealTime,
+        uint256 _endTime
+    );
     event userReceivedRefund(address userAddress, uint256 funds);
 
     error auctionAlreadyExist();
@@ -20,6 +24,7 @@ contract AuctionVickrey is Ownable {
     error notEnoughFunds();
     error refundFailed();
     error auctionInProgress();
+    error auctionIsNotExist();
 
     struct Auction {
         //@notice user's address => hash of user's bid (keccak256(ammount, secretPhrase))
@@ -28,22 +33,22 @@ contract AuctionVickrey is Ownable {
         mapping(address => uint256) bids;
         //@notice user's address => total user sended funds
         mapping(address => uint256) userFunds;
-
         address currentWinner;
-
-        uint256 auctionId;
         uint256 startTime;
         uint256 revealTime;
         uint256 endTime;
         uint256 maxBid;
         uint256 preMaxBid;
-        
         bool isExist;
     }
 
-    mapping(uint256 => Auction) private auctions;
+    mapping(uint256 => Auction) internal auctions;
 
-    function createAuction(uint256 _auctionId, uint256 _revealTime, uint256 _endTime) external onlyOwner {
+    function createAuction(
+        uint256 _auctionId,
+        uint256 _revealTime,
+        uint256 _endTime
+    ) external onlyOwner {
         Auction storage auctionInstance = auctions[_auctionId];
         require(_endTime > _revealTime, incorrectTimeLine());
         require(auctionInstance.isExist == false, auctionAlreadyExist());
@@ -51,24 +56,32 @@ contract AuctionVickrey is Ownable {
         auctionInstance.revealTime = block.timestamp + _revealTime;
         auctionInstance.endTime = block.timestamp + _endTime;
         auctionInstance.isExist = true;
-        auctionInstance.auctionId = _auctionId;
-        emit auctionIsCreated(_auctionId, auctionInstance.startTime, _revealTime, _endTime);
+        emit auctionIsCreated(
+            _auctionId,
+            auctionInstance.startTime,
+            _revealTime,
+            _endTime
+        );
     }
 
     function setBid(uint256 _auctionId, bytes32 hashedBid) external payable {
         Auction storage auctionInstance = auctions[_auctionId];
         require(auctionInstance.isExist == true, auctionIsntExist());
         require(block.timestamp < auctionInstance.endTime, auctionIsEnded());
-        require(block.timestamp < auctionInstance.revealTime, auctionIsOnRevealPhase());
+        require(block.timestamp < auctionInstance.revealTime,auctionIsOnRevealPhase());
         auctionInstance.hashedBids[msg.sender] = hashedBid;
         auctionInstance.userFunds[msg.sender] = msg.value;
     }
 
-    function revealBid(uint256 _auctionId, uint256 bidAmmount, string calldata secretPhrase) external {
+    function revealBid(
+        uint256 _auctionId,
+        uint256 bidAmmount,
+        string calldata secretPhrase
+    ) external {
         Auction storage auctionInstance = auctions[_auctionId];
         require(block.timestamp < auctionInstance.endTime, auctionIsEnded());
         require(auctionInstance.hashedBids[msg.sender] == keccak256(abi.encodePacked(bidAmmount, secretPhrase)), invalidHashedBid());
-        require(auctionInstance.userFunds[msg.sender] >= bidAmmount, notEnoughFunds());
+        require(auctionInstance.userFunds[msg.sender] >= bidAmmount,notEnoughFunds());
         auctionInstance.bids[msg.sender] = bidAmmount;
         if (bidAmmount > auctionInstance.maxBid) {
             auctionInstance.maxBid = bidAmmount;
@@ -81,13 +94,19 @@ contract AuctionVickrey is Ownable {
         Auction storage auctionInstance = auctions[_auctionId];
         require(block.timestamp > auctionInstance.endTime, auctionInProgress());
         if (msg.sender == auctionInstance.currentWinner) {
-            (bool success, ) = payable(msg.sender).call{value: auctionInstance.userFunds[msg.sender] - auctionInstance.preMaxBid}("");
+            (bool success, ) = payable(msg.sender).call{ value: auctionInstance.userFunds[msg.sender] - auctionInstance.preMaxBid }("");
             require(success, refundFailed());
-            emit userReceivedRefund(msg.sender, auctionInstance.userFunds[msg.sender] - auctionInstance.preMaxBid);
+            emit userReceivedRefund(
+                msg.sender,
+                auctionInstance.userFunds[msg.sender] - auctionInstance.preMaxBid
+            );
         } else {
-            (bool success, ) = payable(msg.sender).call{value: auctionInstance.userFunds[msg.sender]}("");
+            (bool success, ) = payable(msg.sender).call{ value: auctionInstance.userFunds[msg.sender] }("");
             require(success, refundFailed());
-            emit userReceivedRefund(msg.sender, auctionInstance.userFunds[msg.sender]);
+            emit userReceivedRefund(
+                msg.sender,
+                auctionInstance.userFunds[msg.sender]
+            );
         }
     }
 
@@ -96,5 +115,4 @@ contract AuctionVickrey is Ownable {
         require(block.timestamp > auctionInstance.endTime, auctionInProgress());
         return auctionInstance.currentWinner;
     }
-
 }
---
+++ contracts/TestedAuctionVickrey.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+import "./AuctionVickrey.sol";
+
+contract TestedAuctionVickrey is AuctionVickrey {
+
+    function getAuctionInfoById(uint256 _auctionId)
+        external
+        view
+        returns (
+            address currentWinner,
+            uint256 startTime,
+            uint256 revealTime,
+            uint256 endTime,
+            uint256 maxBid,
+            uint256 preMaxBid,
+            bool isExist
+        )
+    {
+        Auction storage auctionInstance = auctions[_auctionId];
+        require(auctionInstance.isExist == true, auctionIsNotExist());
+        return (
+            auctionInstance.currentWinner,
+            auctionInstance.startTime,
+            auctionInstance.revealTime,
+            auctionInstance.endTime,
+            auctionInstance.maxBid,
+            auctionInstance.preMaxBid,
+            auctionInstance.isExist
+        );
+    }
+
+    function getHashedBids(address userAddress, uint256 _auctionId) view external returns(bytes32) {
+        Auction storage auctionInstance = auctions[_auctionId];
+        return auctionInstance.hashedBids[userAddress];
+    }
+
+    function getBids(address userAddress, uint256 _auctionId) view external returns(uint256) {
+        Auction storage auctionInstance = auctions[_auctionId];
+        return auctionInstance.bids[userAddress];
+    }
+
+    function getUserFunds(address userAddress, uint256 _auctionId) view external returns(uint256) {
+        Auction storage auctionInstance = auctions[_auctionId];
+        return auctionInstance.userFunds[userAddress];
+    }
+}
